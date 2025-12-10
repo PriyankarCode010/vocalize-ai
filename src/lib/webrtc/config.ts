3d11dfc20b0e78cfe152c9c5aae169ad8d441d3a
Priyankar_010_ <balajihaldar@gmail.com>
2025-12-10 14:20:22 +0530

    refactor iceConfig to dynamically include TURN servers and credentials

--- src/lib/webrtc/config.ts
+++ src/lib/webrtc/config.ts
@@ -1,15 +1,21 @@
+const turnServers = [
+  process.env.NEXT_PUBLIC_TURN_URL_UDP,
+  process.env.NEXT_PUBLIC_TURN_URL_TCP,
+  process.env.NEXT_PUBLIC_TURN_URL_TLS,
+  process.env.NEXT_PUBLIC_TURN_URL_TURNS,
+  process.env.NEXT_PUBLIC_TURN_URL,
+].filter(Boolean) as string[]
+
 export const iceConfig: RTCConfiguration = {
   iceServers: [
-    { urls: "stun:stun.l.google.com:19302" },
-    ...(process.env.NEXT_PUBLIC_TURN_URL
-      ? [
-          {
-            urls: process.env.NEXT_PUBLIC_TURN_URL,
-            username: process.env.NEXT_PUBLIC_TURN_USERNAME,
-            credential: process.env.NEXT_PUBLIC_TURN_PASSWORD,
-          },
-        ]
-      : []),
+    // STUN(s)
+    { urls: process.env.NEXT_PUBLIC_STUN_URL || "stun:stun.l.google.com:19302" },
+    // TURN(s)
+    ...turnServers.map((urls) => ({
+      urls,
+      username: process.env.NEXT_PUBLIC_TURN_USERNAME,
+      credential: process.env.NEXT_PUBLIC_TURN_PASSWORD,
+    })),
   ],
 }
 
